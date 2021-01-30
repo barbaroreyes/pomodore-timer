28ac67569b861340482b033afbc48e057b000748
barbarof <barbarofreyes@gmail.com>
2021-01-29 22:35:44 -0600

    funcions

--- src/pomodore/pomodore.js
+++ src/pomodore/pomodore.js
@@ -1,161 +1,121 @@
 import React, { useState } from "react";
-import classNames from "./utils/class-names";
-import useInterval from "./utils/useInterval";
+//import classNames from "../utils/class-names";
+import useInterval from "../utils/useInterval";
+import Duration from "./Duration";
+import Play from "./Play";
+import Timer from "./Timer";
 
 function Pomodoro() {
+  const initialState = {
+    Focus: 25,
+    Break: 5,
+    remaining: 1500,
+    running: false,
+    inSession: false,
+    currentSession: "Focusing",
+  };
   // Timer starts out paused
-  const [isTimerRunning, setIsTimerRunning] = useState(false);
-  const [focusUp,setFocusUp] = useState(25)
-  const [breackUp,setbreackUp] = useState(5)
+  const [session, setSession] = useState(initialState);
+
+  const handleDurationChange = (type, change) => {
+    setSession((currentSession) => ({
+      ...currentSession,
+      [type]: session[type] + change,
+    }));
+    if (type === "Focus") {
+      setSession((currentSession) => ({
+        ...currentSession,
+        remaining: currentSession.Focus * 60,
+      }));
+    }
+  };
+
+  const resetSession = () => {
+    setSession(initialState);
+  };
 
   useInterval(
     () => {
       // ToDo: Implement what should happen when the timer is running
+      setSession((oldSession) => {
+        return { ...oldSession, remaining: oldSession.remaining - 1 };
+      });
+      if (session.remaining <= 0) {
+        new Audio(
+          `${process.env.PUBLIC_URL}/alarm/Creepy-clock-chiming.mp3`
+        ).play();
+        setSession((oldSession) => {
+          if (oldSession.currentSession === "Focusing") {
+            return {
+              ...oldSession,
+              currentSession: "On Break",
+              remaining: session.Break * 60,
+            };
+          } else {
+            return {
+              ...oldSession,
+              currentSession: "Focusing",
+              remaining: session.Focus * 60,
+            };
+          }
+        });
+      }
     },
-    isTimerRunning ? 1000 : null
+    session.running ? 1000 : null
   );
 
   function playPause() {
-    setIsTimerRunning((prevState) => !prevState);
+    setSession({ ...session, running: !session.running, inSession: true });
   }
 
   return (
-    <div className="pomodoro">
-      <div className="row">
-        <div className="col">
-          <div className="input-group input-group-lg mb-2">
-            <span className="input-group-text" data-testid="duration-focus">
-              {/* TODO: Update this text to display the current focus session duration */}
-              Focus Duration: 25:00
-            </span>
-            <div className="input-group-append">
-              {/* TODO: Implement decreasing focus duration and disable during a focus or break session */}
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-testid="decrease-focus"
-                onClick ={() => setFocusUp(focusUp - 1)}
-              >
-                <span className="oi oi-minus" />
-                down
-              </button>
-              <br/>
-              {focusUp}
-              <br/>
-              {/* TODO: Implement increasing focus duration  and disable during a focus or break session */}
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-testid="increase-focus"
-                onClick ={() => setFocusUp(focusUp + 1)}
-              >
-                <span className="oi oi-plus" />
-               uP
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="float-right">
-            <div className="input-group input-group-lg mb-2">
-              <span className="input-group-text" data-testid="duration-break">
-                {/* TODO: Update this text to display the current break session duration */}
-                Break Duration: 05:00
-              </span>
-              <div className="input-group-append">
-                {/* TODO: Implement decreasing break duration and disable during a focus or break session*/}
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-testid="decrease-break"
-                  onClick={() => setbreackUp(breackUp -1)}
-                >
-                  <span className="oi oi-minus" />
-                  down
-                </button>
-                <br/>
-                {breackUp}
-                <br/>
-                {/* TODO: Implement increasing break duration and disable during a focus or break session*/}
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-testid="increase-break"
-                  onClick={() => setbreackUp(breackUp + 1)}
-                >
-                  <span className="oi oi-plus" />
-                  up
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <div
-            className="btn-group btn-group-lg mb-2"
-            role="group"
-            aria-label="Timer controls"
-          >
-            <button
-              type="button"
-              className="btn btn-primary"
-              data-testid="play-pause"
-              title="Start or pause timer"
-              onClick={playPause}
-            >
-              <span
-                className={classNames({
-                  oi: true,
-                  "oi-media-play": !isTimerRunning,
-                  "oi-media-pause": isTimerRunning,
-                })}
-              />
-              Play
-            </button>
-            {/* TODO: Implement stopping the current focus or break session and disable when there is no active session */}
-            <button
-              type="button"
-              className="btn btn-secondary"
-              title="Stop the session"
-            >
-              <span className="oi oi-media-stop" />
-              Stop
-            </button>
-          </div>
-        </div>
-      </div>
-      <div>
-        {/* TODO: This area should show only when a focus or break session is running or pauses */}
-        <div className="row mb-2">
+    <div>
+      <div className="pomodoro">
+        <div className="row">
           <div className="col">
-            {/* TODO: Update message below to include current session (Focusing or On Break) and total duration */}
-            <h2 data-testid="session-title">Focusing for 25:00 minutes</h2>
-            {/* TODO: Update message below to include time remaining in the current session */}
-            <p className="lead" data-testid="session-sub-title">
-              25:00 remaining
-            </p>
+            <Duration
+              type="Focus"
+              time={session.Focus}
+              setTime={handleDurationChange}
+              high={60}
+              low={5}
+              inSession={session.inSession}
+              increment={5}
+            />
           </div>
-        </div>
-        <div className="row mb-2">
           <div className="col">
-            <div className="progress" style={{ height: "20px" }}>
-              <div
-                className="progress-bar"
-                role="progressbar"
-                aria-valuemin="0"
-                aria-valuemax="100"
-                aria-valuenow="0" // TODO: Increase aria-valuenow as elapsed time increases
-                style={{ width: "0%" }} // TODO: Increase width % as elapsed time increases
+            <div className="float-right">
+              <Duration
+                type="Break"
+                time={session.Break}
+                setTime={handleDurationChange}
+                high={15}
+                low={1}
+                inSession={session.inSession}
+                increment={1}
               />
             </div>
           </div>
         </div>
+        <div className="row">
+          <Play
+            isTimerRunning={session.running}
+            playPause={playPause}
+            resetSession={resetSession}
+            inSession={session.inSession}
+          />
+        </div>
+        <Timer
+          focusTime={session.Focus}
+          breakTime={session.Break}
+          remainingTime={session.remaining}
+          isTimerRunning={session.running}
+          inSession={session.inSession}
+          currentSession={session.currentSession}
+        />
       </div>
     </div>
   );
 }
 
 export default Pomodoro;
-
