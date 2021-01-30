a6a2a7432fc33a7cb888332ef5895a4eca4db24b
barbarof <barbarofreyes@gmail.com>
2021-01-30 10:56:53 -0600

    added animation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-// import Pomodore from './pomodore/pomodore'
-import Pomodore2 from './Pomodore@/pomodore@'
+import Pomodore from './pomodore/pomodore'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          
-         <Pomodore2/>
+          <Pomodore/>
+        <img src={logo} className="App-one" alt="logo" /> 
       </header>
+      
     </div>
   );
 }
 
 export default App;
-// <Pomodore/>
